1e3feadd4d3362c39989436d4af1f4ea42f443bc
jkeary <jkeary@gmail.com>
2021-01-27 16:28:27 -0500

    movement!  no z axis yet, jsut 2d, but still starting to look like something!

--- starfield.js
+++ starfield.js
@@ -1,16 +1,13 @@
 console.log("Let's get to work!");
 
-// const star = document.querySelector('.star');
-const starField = document.querySelector('.star-field');
-console.log(starField)
-// star.addEventListener("load", addMovement()); 
+const starFieldEl = document.querySelector('.star-field');
 
-// function addMovement() {
-//     star.classList.add("star-move");
-// }
+// creating a star field object
+/*
+ Star Object
+   coords: start x, start y, finish x, finish y,
+*/
 
-// creating a star field
-// each start needs a starting x, starting y, ending x and ending y.
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -20,19 +17,84 @@ function getRandomInt(min, max) {
 let starCount = 0;
 let initStarField = []
 while (starCount < 1000) {
-    initStarField.push({x: getRandomInt(0, 100), y: getRandomInt(0, 100)});
+    // y coordinate starts in top left at 0 and moving down is positive (a little confusing)
+    // x coordinate starts top left as well and moves right for positive which follows general cartesian coordinates.
+    // z coordinates the higher the number the further away it appears.  The numbers should always shrink so they seem like theyre coming at us.
+    let startX = getRandomInt(0, 100);
+    let startY = getRandomInt(0, 100);
+    let startZ = getRandomInt(0, 100);
+    let finishX;
+    let finishY;
+    let finishZ = 1;
+    let quadrant;
+    // move up and right
+    if (startX > 50 && startY <= 50) {
+        quadrant = 'upper-right';
+        // one coord has to go to max and the other does not.  Maybe do it half the time one way and half the time the other
+        if (starCount % 2) {
+            finishY = -10; // up is min (set to negative 10 since there seems to be some issue with borders)
+            finishX = getRandomInt(startX, 100); // right is random
+        } else {
+            finishY = getRandomInt(0, startY); // up is random
+            finishX = 100; // right is max
+        }
+    }
+    // move up and left
+    if (startX <= 50 && startY <= 50) {
+        quadrant = 'upper-left';
+
+        if (starCount % 2) {
+            finishY = -10; // up is min
+            finishX = getRandomInt(0, startX); // left is rand
+        } else {
+            finishY = getRandomInt(0, startY); // up is rand
+            finishX = -10; // left is min
+        }
+        
+        
+    }
+    // move down and right
+    if (startX > 50 && startY > 50) {
+        quadrant = 'lower-right';
+        if (starCount % 2) {
+            finishY = 100; // down is max
+            finishX = getRandomInt(startX, 100); // right is rand
+        } else {
+            finishY = getRandomInt(startY, 100); // down is rand
+            finishX = 100; // right is max
+        }
+        
+    }
+    // move down and left
+    if (startX <= 50 && startY > 50) {
+        quadrant = 'lower-left';
+        if (starCount % 2) {
+            finishY = 100; // down is max
+            finishX = getRandomInt(0, startX); // left is rand
+        } else {
+            finishY = getRandomInt(startY, 100); // down is rand
+            finishX = -10; // left is min
+        }
+        
+        
+    }
+
+    initStarField.push({startX, startY, startZ, finishX, finishY, finishZ, quadrant});
     ++starCount;
 } 
 
+console.log(initStarField);
+
 // add those to the dom on start
-initStarField.forEach(star => {
-    console.log(starField);
-    starField.insertAdjacentHTML('afterbegin', `<div class="star" style="transform: translate3d(${star.x}vw, ${star.y}vh, 5px)"></div>`);
+initStarField.forEach((star, i) => {
+    starFieldEl.insertAdjacentHTML('afterbegin', `<div class="star star-${i}" style="transform: translate3d(${star.startX}vw, ${star.startY}vh, 5px)"></div>`);
 });
 
-
 // then move the stars outward
-
-// want random directions
-
-// create a bunch of stars
+// based on the x y coordinates, first determine which quadrant the star is in.
+window.addEventListener('load', () => {
+    initStarField.forEach((star, i) => {
+        const starEl = document.querySelector(`.star-${i}`);
+        starEl.style.transform = `translate3d(${star.finishX}vw, ${star.finishY}vh, 5px)`;
+    })
+});
