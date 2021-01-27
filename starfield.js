2103b51f270d28fc4cedb9c70aaf21e80b036427
jkeary <jkeary@gmail.com>
2021-01-27 18:10:15 -0500

    perfecting the outward motion and adding opacity transition

--- starfield.js
+++ starfield.js
@@ -25,7 +25,7 @@ while (starCount < 1000) {
     let finishX;
     let finishY;
 
-    let scaleStart = getRandomInt(1, 5);
+    let scaleStart = getRandomInt(0, 5);
     let scaleFinish = getRandomInt(5, 10);
     
     let quadrant;
@@ -38,7 +38,7 @@ while (starCount < 1000) {
             finishX = getRandomInt(startX, 100); // right is random
         } else {
             finishY = getRandomInt(0, startY); // up is random
-            finishX = 100; // right is max
+            finishX = 110; // right is max
         }
     }
     // move up and left
@@ -59,11 +59,11 @@ while (starCount < 1000) {
     if (startX > 50 && startY > 50) {
         quadrant = 'lower-right';
         if (starCount % 2) {
-            finishY = 100; // down is max
+            finishY = 110; // down is max
             finishX = getRandomInt(startX, 100); // right is rand
         } else {
             finishY = getRandomInt(startY, 100); // down is rand
-            finishX = 100; // right is max
+            finishX = 110; // right is max
         }
         
     }
@@ -71,7 +71,7 @@ while (starCount < 1000) {
     if (startX <= 50 && startY > 50) {
         quadrant = 'lower-left';
         if (starCount % 2) {
-            finishY = 100; // down is max
+            finishY = 110; // down is max
             finishX = getRandomInt(0, startX); // left is rand
         } else {
             finishY = getRandomInt(startY, 100); // down is rand
@@ -86,8 +86,9 @@ while (starCount < 1000) {
 console.log(initStarField);
 
 // add those to the dom on start
+const speed = ['slow', 'medium', 'fast'];
 initStarField.forEach((star, i) => {
-    starFieldEl.insertAdjacentHTML('afterbegin', `<div class="star star-${i}" style="transform: translate3d(${star.startX}vw, ${star.startY}vh, 5px) scale(${star.scaleStart})"></div>`);
+    starFieldEl.insertAdjacentHTML('afterbegin', `<div class="star star-${i}" style="transition: all ${getRandomInt(0, 7)}s linear; transform: translate3d(${star.startX}vw, ${star.startY}vh, 5px) scale(${star.scaleStart})"></div>`);
 });
 
 // then move the stars outward
@@ -96,5 +97,6 @@ window.addEventListener('load', () => {
     initStarField.forEach((star, i) => {
         const starEl = document.querySelector(`.star-${i}`);
         starEl.style.transform = `translate3d(${star.finishX}vw, ${star.finishY}vh, 5px) scale(${star.scaleFinish})`;
+        starEl.style.opacity = 1;
     })
 });
